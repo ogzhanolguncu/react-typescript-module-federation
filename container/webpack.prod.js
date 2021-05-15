5850455eba895674f8a969dbef78a4ec24bf23de
ogzhanolguncu <ogzhan11@gmail.com>
2021-05-15 03:21:37 +0300

    final-trigger

--- container/webpack.prod.js
+++ container/webpack.prod.js
@@ -1,18 +1,10 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { ModuleFederationPlugin } = require('webpack').container;
-const path = require('path');
 const deps = require('./package.json').dependencies;
 
 module.exports = {
   entry: './src/index.ts',
   mode: 'development',
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    port: 3000,
-  },
-  output: {
-    publicPath: 'http://localhost:3000/',
-  },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
   },
@@ -44,7 +36,7 @@ module.exports = {
       },
     }),
     new HtmlWebpackPlugin({
-      template: './public/index.dev.html',
+      template: './public/index.prod.html',
     }),
   ],
 };
