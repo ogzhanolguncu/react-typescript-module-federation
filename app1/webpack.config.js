2800c84aeb0580855a9b7d6d5f5bbd889c748cad
Serhat Genc <serhat.genc@fireflyon.com>
2022-03-20 11:57:56 +0300

    container loader changed to babel-loader and headers added each project

--- app1/webpack.config.js
+++ app1/webpack.config.js
@@ -4,44 +4,47 @@ const path = require("path");
 const deps = require("./package.json").dependencies;
 
 module.exports = {
-	entry: "./src/index.ts",
-	mode: "development",
-	devServer: {
-		port: 3001,
-		open: true,
-	},
-	resolve: {
-		extensions: [".ts", ".tsx", ".js"],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx|tsx|ts)$/,
-				loader: "ts-loader",
-				exclude: /node_modules/,
-			},
-		],
-	},
-	plugins: [
-		new ModuleFederationPlugin({
-			name: "app1",
-			filename: "remoteEntry.js",
-			exposes: {
-				// expose each component
-				"./CounterAppOne": "./src/components/CounterAppOne",
-			},
-			shared: {
-				...deps,
-				react: { singleton: true, eager: true, requiredVersion: deps.react },
-				"react-dom": {
-					singleton: true,
-					eager: true,
-					requiredVersion: deps["react-dom"],
-				},
-			},
-		}),
-		new HtmlWebpackPlugin({
-			template: "./public/index.html",
-		}),
-	],
+  entry: "./src/index.ts",
+  mode: "development",
+  devServer: {
+    port: 3001,
+    open: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|tsx|ts)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "app1",
+      filename: "remoteEntry.js",
+      exposes: {
+        // expose each component
+        "./CounterAppOne": "./src/components/CounterAppOne",
+      },
+      shared: {
+        ...deps,
+        react: { singleton: true, eager: true, requiredVersion: deps.react },
+        "react-dom": {
+          singleton: true,
+          eager: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
--- app2/webpack.config.js
+++ app2/webpack.config.js
@@ -4,44 +4,47 @@ const path = require("path");
 const deps = require("./package.json").dependencies;
 
 module.exports = {
-	entry: "./src/index.ts",
-	mode: "development",
-	devServer: {
-		port: 3002,
-		open: true,
-	},
-	resolve: {
-		extensions: [".ts", ".tsx", ".js"],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx|tsx|ts)$/,
-				loader: "ts-loader",
-				exclude: /node_modules/,
-			},
-		],
-	},
-	plugins: [
-		new ModuleFederationPlugin({
-			name: "app2",
-			filename: "remoteEntry.js",
-			exposes: {
-				// expose each component
-				"./CounterAppTwo": "./src/components/CounterAppTwo",
-			},
-			shared: {
-				...deps,
-				react: { singleton: true, eager: true, requiredVersion: deps.react },
-				"react-dom": {
-					singleton: true,
-					eager: true,
-					requiredVersion: deps["react-dom"],
-				},
-			},
-		}),
-		new HtmlWebpackPlugin({
-			template: "./public/index.html",
-		}),
-	],
+  entry: "./src/index.ts",
+  mode: "development",
+  devServer: {
+    port: 3002,
+    open: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|tsx|ts)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "app2",
+      filename: "remoteEntry.js",
+      exposes: {
+        // expose each component
+        "./CounterAppTwo": "./src/components/CounterAppTwo",
+      },
+      shared: {
+        ...deps,
+        react: { singleton: true, eager: true, requiredVersion: deps.react },
+        "react-dom": {
+          singleton: true,
+          eager: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
